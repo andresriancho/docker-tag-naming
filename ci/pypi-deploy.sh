a35d6a1543de543fdd7e2e04c97c478ca8aa75fc
Andres Riancho <andres.riancho@gmail.com>
2015-07-02 12:31:23 -0300

    Fix deploy

--- ci/pypi-deploy.sh
+++ ci/pypi-deploy.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-set -e
-
 echo "[server-login]" > ~/.pypirc
-echo "username:" $PYPI_USER >> ~/.pypirc
-echo "password:" $PYPI_PASSWORD >> ~/.pypirc
+echo "username:" ${PYPI_USER} >> ~/.pypirc
+echo "password:" ${PYPI_PASSWORD} >> ~/.pypirc
 
-PACKAGE_URL='https://pypi.python.org/packages/source/d/docker-tag-naming/'
+version=`python -c 'from docker_tag_naming import __VERSION__;print __VERSION__,'`
+curl -f -s -S -k -X GET -I https://pypi.python.org/packages/source/d/docker-tag-naming/docker-tag-naming-$version.tar.gz
 
-packages=`curl -f -s -S -k $PACKAGE_URL`
-dtn_version=`python -c 'from docker_tag_naming import __VERSION__;print __VERSION__,'`
-current_package="docker-tag-naming-${dtn_version}.tar.gz"
-
-if [[ $packages == *$current_package* ]]
+if [[ $? -eq 0 ]]
 then
     echo "Current package version is already at PyPi. If your intention was to"
-    echo " release a new version, you'll have to increase the version number."
+    echo "release a new version, you'll have to increase the version number."
 else
-    echo "Uploading $dtn_version version to PyPi"
+    echo "Uploading $version version to PyPi"
     python setup.py sdist upload
 fi
+
+exit 0
